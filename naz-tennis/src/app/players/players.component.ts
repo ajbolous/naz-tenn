215b56d3d540fcb9c3d564e57b52737c4d6aec15
Bolous Jaber <ajbolous@gmail.com>
2019-06-28 13:51:11 +0300

    Added score page

--- naz-tennis/src/app/players/players.component.ts
+++ naz-tennis/src/app/players/players.component.ts
@@ -6,14 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./players.component.css']
 })
 export class PlayersComponent implements OnInit {
- 
-  players: {name:string, description:string, league:string}[] = [
-    {name: "Bolous Jaber", description: "Very good player, Born in nazareth and plays for 10 years.", league:"Nazareth"},
-    {name: "Bolous Jaber", description: "Very good player, Born in nazareth and plays for 10 years.", league:"Nazareth"},
-    {name: "Bolous Jaber", description: "Very good player, Born in nazareth and plays for 10 years.", league:"Nazareth"},
-    {name: "Bolous Jaber", description: "Very good player, Born in nazareth and plays for 10 years.", league:"Nazareth"}
 
+  public men: { name: string, description: string, league: string }[] = [
+    { name: "Bolous AbuJaber", description: "10 Yeras tennis player, Righ handed", league: "Haifa" },
+    { name: "Osama AbuJaber", description: "More than 10 years experience", league: "Haifa" },
+    { name: "Tarek Salloum", description: "A young but promising tennis player", league: "Nazareth" },
+    { name: "Waseem Mazzawi", description: "Excellent tennis player", league: "Nazareth" },
   ];
+
+  public women: { name: string, description: string, league: string }[] = [
+    { name: "Layal Ammouri", description: "The best female tennis player in Nazareth", league: "Nazareth" },
+    { name: "Sharifeh Safadi", description: "One of the greatest of all times", league: "Nazareth" },
+  ];
+
 
   constructor() { }
 
--- naz-tennis/src/app/scores/scores.component.ts
+++ naz-tennis/src/app/scores/scores.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScoresComponent implements OnInit {
 
+  playDates = [
+    { id: 1, day: new Date(1, 7, 2019) },
+    { id: 2, day: new Date(2, 7, 2019) },
+    { id: 3, day: new Date(3, 7, 2019) },
+    { id: 4, day: new Date(4, 7, 2019) },
+    { id: 5, day: new Date(5, 7, 2019) }
+  ]
+
+  scores = [
+    {
+      winner: 'Bolous', loser: 'Tarek', sets: [[6, 4], [6, 3]]
+    },
+    {
+      winner: 'Tarek', loser: 'Tarek', sets: [[6, 4], [2, 6], [6,4]]
+    },
+    {
+      winner: 'Tarek', loser: 'Ward', sets: [[6, 2], [2, 6], [6,1]]
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
